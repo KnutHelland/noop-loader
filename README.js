e8a707ea3ce8e7f47764a23b2893d60c43f01200
Knut Helland <knut@dx.no>
2015-07-29 15:49:05 +0200

    Initial commit

---
+++ README.js
@@ -0,0 +1,4 @@
+# Noop loader for webpack
+
+There are probably other ways to solve these problems, but sometimes I want a
+loader for webpack that does exactly nothing, and that is what this is for.
---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = function(source, map) {
+	this.cacheable();
+	this.callback(null, source, map);
+}
+
